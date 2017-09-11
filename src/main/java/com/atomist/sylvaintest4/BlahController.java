03f223dc740a20eedb62e4df5a663503ebeed213
Sylvain Hellegouarch <sylvain@atomist.com>
2017-09-11 18:43:03 +0200

    add rest controller

---
+++ src/main/java/com/atomist/sylvaintest4/BlahController.java
@@ -0,0 +1,17 @@
+package com.atomist.sylvaintest4;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api")
+public class BlahController {
+    @RequestMapping(value = "/user/", method = RequestMethod.GET)
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<String>("Hi there", HttpStatus.OK);
+    }
+}
